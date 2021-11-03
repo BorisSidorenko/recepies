b1cb1bfa7ed2449b5fcf0c2ecb48f8c0c68bad25
Boris <boris.m.sidorenko@gmail.com>
2021-11-03 22:17:30 +0300

    loaded and displayed recepies

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Create from './pages/create/Create';
 import Home from './pages/home/Home';
 import Recipe from './pages/recipe/Recipe';
 import Search from './pages/search/Search';
+import Navbar from './components/Navbar';
 
 import './App.css'
 
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Switch>
           <Route exact path="/">
             <Home />
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,15 @@
+import { useFetch } from '../../hooks/useFetch';
 import './Home.css';
 
 export default function Home() {
+    const {data: recipes, isPending, error } = useFetch('http://localhost:3000/recipes');
     return (
-        <div>
-            Home
+        <div className="home">
+            {error && <p className="error">{error}</p>}
+            {isPending && <p className="loading">Loading...</p>}
+            {recipes && recipes.map((recipe) => (
+                <h2 key={recipe.id}>{recipe.title}</h2>
+            ))}
         </div>
     )
 }
