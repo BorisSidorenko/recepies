0fad5bea9d365b4ace54778a8f9ad433a270605d
Boris <boris.m.sidorenko@gmail.com>
2021-11-07 21:13:20 +0300

    added changed color function based on context color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom';
 import Searchbar from './Searchbar';
+import { useTheme } from '../hooks/useTheme';
 import './Navbar.css';
 
-export default function Navbar() {
+export default function Navbar() {    
+    const { color } = useTheme();
+
     return (
-        <div className="navbar">
+        <div className="navbar" style={{ background: color }}>
             <nav>
                 <Link to="/" className="brand">
                     <h1>Cooking Ninja</h1>
